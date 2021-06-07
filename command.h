1fa883aa8b6782f34359c1c773782379e3b6d2be
R. van Elst <vanelst@dejongduke.nl>
2021-06-07 21:39:53 +0200

    Initial commit

---
+++ command.h
@@ -0,0 +1,104 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+// Copyright (C) 2021 Remy van Elst
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#include <array>
+#include <ostream>
+#include <string>
+#ifdef _WIN32
+#include <stdio.h>
+#endif
+
+namespace raymii {
+
+    struct CommandResult {
+        std::string output;
+        int exitstatus;
+        friend std::ostream &operator<<(std::ostream &os, const CommandResult &result) {
+            os << "command exitstatus: " << result.exitstatus << " output: " << result.output;
+            return os;
+        }
+        bool operator==(const CommandResult &rhs) const {
+            return output == rhs.output &&
+                   exitstatus == rhs.exitstatus;
+        }
+        bool operator!=(const CommandResult &rhs) const {
+            return !(rhs == *this);
+        }
+    };
+
+    class Command {
+    public:
+        /**
+             * Execute system command and get STDOUT result.
+             * Regular system() only gives back exit status, this gives back output as well.
+             * @param command system command to execute
+             * @return commandResult containing STDOUT (not stderr) output & exitstatus
+             * of command. Empty if command failed (or has no output). If you want stderr,
+             * use shell redirection (2&>1).
+             */
+        static CommandResult exec(const std::string &command) {
+            int exitcode = 255;
+            std::array<char, 4> buffer{};
+            std::string result;
+#ifdef _WIN32
+            FILE *pipe = _popen(command.c_str(), "r");
+#else
+            FILE *pipe = popen(command.c_str(), "r");
+#endif
+            if (pipe == nullptr) {
+                throw std::runtime_error("popen() failed!");
+            }
+            try {
+                std::size_t bytesread;
+                while ((bytesread = std::fread(buffer.data(), sizeof(buffer.at(0)), sizeof(buffer), pipe)) != 0) {
+                    result += std::string(buffer.data(), bytesread);
+                }
+            } catch (...) {
+                pclose(pipe);
+                throw;
+            }
+            exitcode = WEXITSTATUS(pclose(pipe));
+            return CommandResult{result, exitcode};
+        }
+
+        // Only for reference in the article. Use regular ::exec.
+        static CommandResult execFgets(const std::string &command) {
+            int exitcode = 255;
+            std::array<char, 4> buffer{};
+            std::string result;
+#ifdef _WIN32
+            FILE *pipe = _popen(command.c_str(), "r");
+#else
+            FILE *pipe = popen(command.c_str(), "r");
+#endif
+            if (pipe == nullptr) {
+                throw std::runtime_error("popen() failed!");
+            }
+            try {
+                while (std::fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
+                    result += buffer.data();
+                }
+            } catch (...) {
+                pclose(pipe);
+                throw;
+            }
+            exitcode = WEXITSTATUS(pclose(pipe));
+            return CommandResult{result, exitcode};
+        }
+    };
+
+}// namespace raymii
+#endif//COMMAND_H
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "command.h"
+// Copyright (C) 2021 Remy van Elst
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+int main() {
+    using namespace raymii;
+
+    std::string expectedOutput("test\000abc\n", 9); //NOLINT We want to test for nullbytes...
+    CommandResult nullbyteCommand = Command::exec("/usr/bin/printf 'test\\000abc\\n'"); // NOLINT(bugprone-string-literal-with-embedded-nul)
+    CommandResult fgetsNullbyteCommand = Command::execFgets("/usr/bin/printf 'test\\000abc\\n'"); // NOLINT(bugprone-string-literal-with-embedded-nul)
+
+    std::cout << "Expected output: " << expectedOutput << std::endl;
+    std::cout << "Output using fread:  " << nullbyteCommand << std::endl;
+    std::cout << "Output using fgets: " << fgetsNullbyteCommand << std::endl;
+
+    std::cout << "Redirect stderr to stdout" << std::endl;
+    std::cout << Command::exec("/bin/bash --invalid  2>&1") << std::endl;
+
+#ifdef _WIN32
+    std::cout << "Windows example:" << std::endl;
+    std::cout << Command::exec("dir * /on /p") << std::endl;
+#endif
+
+    return 0;
+}
